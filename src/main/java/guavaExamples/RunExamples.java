109d0def6927a3c0400390aa2d883173a1b2831f
Josh Anderson <jbanderson2001@gmail.com>
2023-08-02 00:02:11 +1200

    Added Guava Dependency

--- src/main/java/guavaExamples/RunExamples.java
+++ src/main/java/guavaExamples/RunExamples.java
@@ -3,7 +3,8 @@ package guavaExamples;
 public class RunExamples {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Guava example project. When you have added the dependency, you will see their examples");
+		System.out.println("Welcome to the Guava example project. When you have added " +
+				"the dependency, you will see their examples");
 		
 		// Guava-dependent examples
 		new ImmutablesInGuava().run();
